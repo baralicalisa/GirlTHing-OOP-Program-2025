f6342e612910a470ef1831e40a4ac054cc227386
Alisa Baralić <a.baralic4@gmail.com>
2025-12-25 11:26:45 +0100

    Update zadatak2_alisa_baralic.cpp

--- zadatak2_alisa_baralic.cpp
+++ zadatak2_alisa_baralic.cpp
@@ -5,19 +5,18 @@ Napisati program koji racuna sumu prvih 20 prirodnih brojeva koristenjem for pet
 #include <iostream>
 
 int main(){
-    int n, suma;
-    n=20;
-    suma=n*(n+1)/2;
-    
+    int n=20, suma=0;
+
+    for(int i=1;i<=n;i++){
+        suma+=i;
+    }
     
     /* drugi nacin
-        suma=0;
-        for(int i=1;i<=20;i++){
-            suma+=i;
-        }
+        suma=n*(n+1)/2;
     */
 
     std::cout<<"Suma prvih 20 prirodnih brojeva je "<<suma<<std::endl;
 
     return 0;
+
 }
